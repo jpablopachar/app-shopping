c7a3efc9c8dbb1b04e2c083338634c9e22e7c3f1
jpablopachar <jpablopachar1993@gmail.com>
2023-05-03 16:09:43 -0500

    Se crea contexto para book-service

---
+++ book-service/Persistence/LibraryContext.cs
@@ -0,0 +1,15 @@
+using book_service.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace book_service.Persistence
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions options) : base(options)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+
+        public DbSet<LibraryMaterial>? LibraryMaterial { get; set; }
+    }
+}
