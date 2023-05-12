fec832829e87f8506ab09003eec1e7197fbb3b84
jpablopachar <jpablopachar1993@gmail.com>
2023-05-11 23:44:50 -0500

    Prueba de book-service

---
+++ book-service/Migrations/20230512042901_MyFirstMigration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace book_service.Migrations
+{
+    /// <inheritdoc />
+    public partial class MyFirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LibraryMaterial",
+                columns: table => new
+                {
+                    LibraryMaterialId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PublicationDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    AuthorBook = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LibraryMaterial", x => x.LibraryMaterialId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LibraryMaterial");
+        }
+    }
+}
